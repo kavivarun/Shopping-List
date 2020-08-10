88f7d69f56097a7def85d7082955ac2b98b6d8cc
kavivarun <kavivarun.s@gmail.com>
2020-08-10 15:56:27 +0530

    second commit

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const app = express();
 
 const items = require('./routes/api/items');
 
+const path = require('path')
+
 //Body parser middleware
 app.use(express.json());
 
@@ -15,7 +17,15 @@ db = require('./config/keys.js').mongoURI;
 //DB connection
 
 mongoose.connect(db,{useUnifiedTopology: true, useNewUrlParser: true }).then(() => console.log('Mongo Connected')).catch( err => console.log(err));
+// serve static assets in production
+if(process.env.node_env === 'production'){
+    //set static folder
+    app.use(express.static('client/build'))
 
+    app.get('*',(req,res) => {
+        res.sendfile(path.resolve(__dirname,'client', 'build','index.html'))
+    })
+}
 // Use Routes
 app.use('/api/items',items);
 const port = process.env.PORT || 5000;
